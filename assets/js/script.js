34239287baa5d6a82e42424e6a86c7766e4d8b7c
ccffdead <ccffdead@gmail.com>
2021-10-25 14:53:33 -0400

    form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,15 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
+
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if (!taskNameInput || !taskTypeInput ) {
+        alert("you need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
 
     // package up data as an object
     var taskDataObj = {
